f9974d7b43153b2e93923fb5c188bb801f9438ed
Kieran Hanrahan <kieran@knhn.me>
2025-03-06 14:17:46 -0500

    Update install instructions in docstring

--- continue_folder.py
+++ continue_folder.py
@@ -23,10 +23,13 @@ Menus:
 To Install:
 
     For all users, copy this file to:
-    /opt/Autodesk/shared/python
+    /opt/Autodesk/shared/python/
 
-    For a specific user, copy this file to:
-    /opt/Autodesk/user/<user name>/python
+    For a specific user on Linux, copy this file to:
+    /home/<user_name>/flame/python/
+
+    For a specific user on Mac, copy this file to:
+    /Users/<user_name>/Library/Preferences/Autodesk/flame/python/
 """
 
 
